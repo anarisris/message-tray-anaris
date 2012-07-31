3d47f0a242da297eb51e1bf7d1dc6347ecf15641
Florian Müllner <fmuellner@gnome.org>
2012-07-31 16:10:24 +0200

    message-tray: Add :overview pseudo class when in overview
    The designers would like the message-tray to use different styles
    depending on whether it is shown in the overview or not, so use
    an :overview pseudo class as the top bar.

--- js/ui/messageTray.js
+++ js/ui/messageTray.js
@@ -1526,6 +1526,7 @@ const MessageTray = new Lang.Class({
         Main.overview.connect('showing', Lang.bind(this,
             function() {
                 this._overviewVisible = true;
+                this.actor.add_style_pseudo_class('overview');
                 if (this._locked) {
                     this._unsetClickedSummaryItem();
                     this._unlock();
@@ -1536,6 +1537,7 @@ const MessageTray = new Lang.Class({
         Main.overview.connect('hiding', Lang.bind(this,
             function() {
                 this._overviewVisible = false;
+                this.actor.remove_style_pseudo_class('overview');
                 if (this._locked) {
                     this._unsetClickedSummaryItem();
                     this._unlock();
